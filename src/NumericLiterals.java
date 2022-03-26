f7679706458127b07f7afe0b5561224b20ff841c
Abdul Hadi N <abdulhadi1989@gmail.com>
2022-03-26 17:23:09 +0000

    More programs

---
+++ src/NumericLiterals.java
@@ -0,0 +1,12 @@
+
+public class NumericLiterals {
+
+	public static void main(String[] args) {
+	//	long creditCardNumber = 12345678767345L;
+		
+		long creditCardNumber = 1234_5678_7673_8845L;
+		
+		System.out.println(creditCardNumber);
+		
+	}
+}
---
+++ src/RealPrimitives.java
@@ -0,0 +1,15 @@
+
+public class RealPrimitives {
+	
+	public static void main(String args[]) {
+		
+		float f=1.1f; // a single precision 32 but IEEE 754 floating point
+		double d = 1.1d; //a double precision 64 bit IEEE 754 floating point
+		
+		double d1 = 123.4;
+		double d2 = 1.234e2; //same values as d1, but in scientific notation
+		float f1 = 123.4f;
+		System.out.println("Output =" +d1+"  " +f1+"  ");
+	}
+
+}
